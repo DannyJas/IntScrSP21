aec0d12e82ae65dc8fa1a5c20d0da62e01a03bb1
Daniel Jasinski <djas1229@gmail.com>
2021-02-16 17:30:27 -0600

    Added the Clock tutorial

---
+++ Assets/Clock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+	const float hoursToDegrees = -30, minutesToDegrees =-6f, secondstoDegress = -6f; 
+
+	[SerializeField]
+		Transform hoursPivot = default, minutesPivot, secondsPivot;
+    void Update () {
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		//Debug.Log (DateTime.Now.Hour); 
+	       hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float) time.TotalHours); 
+		   minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float) time.TotalMinutes);
+		   secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondstoDegress * (float) time.TotalSeconds); 
+
+
+	}
+}
